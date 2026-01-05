86b175ea4ceae8b012f02378d013f235ce50a3ce
Pujita nagalakshmi Kotha <pujitha0709@gmail.com>
2026-01-05 21:45:38 +0530

    Create upgrade-to-v2.js

---
+++ scripts/upgrade-to-v2.js
@@ -0,0 +1,33 @@
+const { ethers, upgrades } = require('hardhat');
+
+async function main() {
+  const PROXY_ADDRESS = process.env.PROXY_ADDRESS;
+  if (!PROXY_ADDRESS) {
+    throw new Error('Please provide PROXY_ADDRESS environment variable');
+  }
+  
+  console.log('Upgrading TokenVault from V1 to V2...');
+  console.log('Proxy address:', PROXY_ADDRESS);
+  
+  const TokenVaultV2 = await ethers.getContractFactory('TokenVaultV2');
+  const upgraded = await upgrades.upgradeProxy(PROXY_ADDRESS, TokenVaultV2);
+  
+  console.log('TokenVaultV2 upgraded successfully at:', upgraded.address);
+  console.log('New implementation version:', await upgraded.getImplementationVersion());
+  
+  const implAddress = await upgrades.erc1967.getImplementationAddress(upgraded.address);
+  console.log('New implementation address:', implAddress);
+  
+  return {
+    proxyAddress: upgraded.address,
+    implementationAddress: implAddress,
+    version: await upgraded.getImplementationVersion()
+  };
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
